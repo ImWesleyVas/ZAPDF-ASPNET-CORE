b6a21853b4e30a6ddcc59e7bb93f259515dcaf0c
ImWesleyVas <souwesleyroque@gmail.com>
2021-05-30 13:48:39 -0300

    Improving navigation more

--- ZAPNET.DemoFina/wwwroot/js/conta-df.js
+++ ZAPNET.DemoFina/wwwroot/js/conta-df.js
@@ -3,26 +3,45 @@
 
     $(window).on("load", function (evento) {
         evento.preventDefault();
-
         debugger;
 
         carregaMesAno(".lblMesAno");
 
-        var btnRela = $('a.btn.btn-default.glyphicon.glyphicon-triangle-bottom');
+        var btnRela = $('button.btn.btn-default.glyphicon.glyphicon-triangle-bottom');
 
         for (var i = 0; i < btnRela.length; i++) {
-            
+
             var pai = btnRela[i].parentElement;
             var avo = pai.parentElement;
+            var bisavo = avo.parentElement;
+
             var titioAvo = avo.previousElementSibling
                 .previousElementSibling
                 .previousElementSibling
+                .previousElementSibling
                 .previousElementSibling;
-            
+
             if (titioAvo.textContent.trim() == "S") {
                 btnRela[i].classList.add('btn-rela-none');
+                var titioBisavo = bisavo.nextElementSibling;
+                titioBisavo.remove();
             }
         }
+
+    })
+
+    $('[data-btn-rela]').on('click', function (event) {
+        event.preventDefault();
+        /*debugger;*/
+        var paiButton = this.parentElement;
+        var avoButton = paiButton.parentElement;
+        var bisaboButton = avoButton.parentElement;
+        var titioBisavo = bisaboButton.nextElementSibling;
+        
+        titioBisavo.classList.toggle('data-add-cosif-show');
+
+        console.log(titioBisavo);
+
     })
 
 })()
